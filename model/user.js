9bf1b767de746c2ef93d8df5b6bb2180919dc669
ebrahim-kbn <ebrahim.kbn@gmail.com>
2020-04-05 17:04:14 +0430

    initial commit;

---
+++ model/user.js
@@ -0,0 +1,40 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        maxlength:50,
+    },
+    email:{
+        type:String,
+        trim:true,
+        unique:1,
+    },
+    password:{
+        type:String,
+        minlength:5,
+    },
+    lastname:{
+        type:String,
+        maxlength:50,
+    },
+    role:{
+        type:Number,
+        default:0,
+    },
+    token:{
+        type:String,
+    },
+    tokenExp:{
+        type:Number,
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+
+
+module.exports=mongoose.model('User',userSchema)
